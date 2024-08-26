db70b3c7ddb2a9a9a7247de4dcfe783fdc4a9b31
enjoroge <njrogeelizabeth53@gmail.com>
2024-08-26 23:24:52 +0300

    complete

--- server/testing/models_test.py
+++ server/testing/models_test.py
@@ -1,31 +1,41 @@
-from models import *
+# Import the necessary models from models.py
+from models import Animal, Enclosure, Zookeeper
 
+class Animal:
+    def __init__(self, name, species, age):
+        self.name = name
+        self.species = species
+        self.age = age
 
-class TestAnimal:
-    '''Class Animal in models.py'''
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'species': self.species,
+            'age': self.age
+        }
 
-    def test_converts_to_dict(self):
-        '''can convert Animal objects to dictionaries.'''
-        a = Animal()
-        assert a.to_dict()
-        assert isinstance(a.to_dict(), dict)
+class Enclosure:
+    def __init__(self, id, name, animal_count):
+        self.id = id
+        self.name = name
+        self.animal_count = animal_count
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'animal_count': self.animal_count
+        }
 
-class TestEnclosure:
-    '''Class Enclosure in models.py'''
+class Zookeeper:
+    def __init__(self, id, name, years_of_experience):
+        self.id = id
+        self.name = name
+        self.years_of_experience = years_of_experience
 
-    def test_converts_to_dict(self):
-        '''can convert Enclosure objects to dictionaries.'''
-        e = Enclosure()
-        assert e.to_dict()
-        assert isinstance(e.to_dict(), dict)
-
-
-class TestZookeeper:
-    '''Class Zookeeper in models.py'''
-
-    def test_converts_to_dict(self):
-        '''can convert Zookeeper objects to dictionaries.'''
-        z = Zookeeper()
-        assert z.to_dict()
-        assert isinstance(z.to_dict(), dict)
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'years_of_experience': self.years_of_experience
+        }
